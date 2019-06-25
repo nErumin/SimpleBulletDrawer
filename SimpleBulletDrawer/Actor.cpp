f4b4023472bb9e529bf770b2d291d3180dce2d84
nErumin <nerumin90@gmail.com>
2019-06-25 17:49:18 +0900

    Adjust spacing in the Actor.

--- SimpleBulletDrawer/Actor.cpp
+++ SimpleBulletDrawer/Actor.cpp
@@ -29,8 +29,6 @@ glm::mat4 Actor::TransformationMatrix() const
 
     // Rotate
     transformationMatrix = glm::rotate(transformationMatrix, glm::radians(mRotation.x), glm::vec3{ 1, 0, 0 });
-
-
     transformationMatrix = glm::rotate(transformationMatrix, glm::radians(mRotation.y), glm::vec3{ 0, 1, 0 });
     transformationMatrix = glm::rotate(transformationMatrix, glm::radians(mRotation.z), glm::vec3{ 0, 0, 1 });
 
